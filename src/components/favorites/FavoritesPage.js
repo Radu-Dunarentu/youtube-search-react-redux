3c3d47e9a888b6ffda7de84b4677947173859ddc
Radu Dunarentu <radu@netop.com>
2017-09-06 10:26:51 +0300

    feat(app): testing pretifier

--- src/components/favorites/FavoritesPage.js
+++ src/components/favorites/FavoritesPage.js
@@ -1,47 +1,53 @@
 /**
  * Created by Radu on 25.06.2017.
  */
-import React from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import {connect} from 'react-redux';
-import {withRouter} from 'react-router-dom';
-import * as favoriteAction from '../../actions/favoriteAction';
-import toastr from 'toastr';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import * as favoriteAction from "../../actions/favoriteAction";
+import toastr from "toastr";
 
 class SearchPage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = {
-    };
+    this.state = {};
   }
-
 
   render() {
     return (
       <div>
         <h1>Favorites</h1>
-        <hr/>
-        {this.props.favorites.length > 0 && <table className="table">
-        <thead>
-        <tr>
-          <th>Title</th>
-          <th>Action</th>
-        </tr>
-        </thead>
-        <tbody>
-        {this.props.favorites.map(item => (
-          <tr key={item.id}>
-            <td>{item.title}</td>
-            <td><button type="button" className="btn btn-secondary" onClick={() =>
-            this.props.actions.removeFromFavorite(item)}>Remove</button></td>
-          </tr>
-        ))}
-        </tbody>
-      </table>}
-
+        <hr />
+        {this.props.favorites.length > 0 && (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.favorites.map(item => (
+                <tr key={item.id}>
+                  <td>{item.title}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={() =>
+                        this.props.actions.removeFromFavorite(item)}
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     );
   }
@@ -64,4 +70,6 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchPage));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(SearchPage)
+);
